0ad01f9a323d78d79ca554f15a089b693a50e3e1
SPoon <poonselinasl@gmail.com>
2021-04-26 00:56:35 -0500

    First commit

---
+++ src/test/java/org/launchcode/habittracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
