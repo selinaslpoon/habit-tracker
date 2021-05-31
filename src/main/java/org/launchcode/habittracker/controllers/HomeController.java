090daeac23280a230c480c3af6fbcd55ba972118
selinaslpoon <poonselinasl@gmail.com>
2021-05-31 02:28:48 -0500

    Tracker data: checkboxes can READ from tracker repo

--- src/main/java/org/launchcode/habittracker/controllers/HomeController.java
+++ src/main/java/org/launchcode/habittracker/controllers/HomeController.java
@@ -15,18 +15,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-//Toggle tokens: https://uxmovement.com/forms/why-toggle-tokens-are-a-better-alternative-to-checkboxes/
-//https://getbootstrap.com/docs/4.1/components/buttons/
-//https://www.codejava.net/frameworks/spring-boot/spring-thymeleaf-form-multi-checkboxes-mapping-with-collection-example
-
-//LocalDate: https://www.geeksforgeeks.org/localdate-of-method-in-java-with-examples/
-//CRUD repository methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
-//composite primary key example: https://www.callicoder.com/hibernate-spring-boot-jpa-composite-primary-key-example/
-
 @Controller
-//@RequestMapping("")
 public class HomeController {
-
 
     @Autowired
     HabitRepository habitRepository;
@@ -35,89 +25,24 @@ public class HomeController {
     TrackerRepository trackerRepository;
 
     static List<LocalDate> dates = new ArrayList<>();
-    static LocalDate newDate = LocalDate.of(2021, 5, 1);
+    static LocalDate newDate = LocalDate.of(2021, 6, 1);
+    static List<Habit> allHabits = new ArrayList<>();
+//    static List<Boolean> isComplete = new ArrayList<>();
+    static ArrayList<ArrayList<Boolean>> isComplete = new ArrayList<>();
+    static Integer foundComplete;
 
     public HomeController() {
 
         //temporary list of dates
         //initialise date
-        LocalDate newDate = LocalDate.of(2021, 5, 1);
+        LocalDate newDate = LocalDate.of(2021, 6, 1);
         for (long i = 0; i < newDate.lengthOfMonth(); i++ ) {
             dates.add(newDate.plusDays(i));
         }
-
-//        Habit newHabit = new Habit("Test");
-//        habitRepository.save(newHabit);
-//        Tracker newTracker = new Tracker(newHabit, newDate);
-//        trackerRepository.save(newTracker);
-
-        //testing tracker repository
-//
-//        Habit testHabit = new Habit("Test2");
-//        habitRepository.save(testHabit);
-//
-//        Habit aHabit = habitRepository.findById(testHabit.getId());
-
-//        LocalDate testDate = LocalDate.of(2021, 5, 1);
-//        Tracker testTracker = new Tracker(testHabit, testDate);
-//        trackerRepository.save(testTracker);
-
     }
 
     @GetMapping
     public String displayAllHabits(Model model) {
-
-//        //test for creating and saving new habits AND TRACKER
-//        Habit newHabit = new Habit("second tracker test");
-//        habitRepository.save(newHabit);
-//        Tracker newTracker = new Tracker();
-//        newTracker.setHabit(newHabit);
-//        LocalDate testDate = LocalDate.of(2021, 5, 1);
-//        newTracker.setDate(testDate);
-//        trackerRepository.save(newTracker);
-
-
-//        //delete tracker by ID
-//        Optional<Tracker> delTracker = trackerRepository.findById(114);
-//        if (!delTracker.isEmpty()) {
-//            trackerRepository.deleteById(delTracker.get().getId());
-//        }
-//
-
-//        //delete habit by ID
-//        Optional<Habit> delHabit = habitRepository.findById(113);
-//        if (!delHabit.isEmpty()) {
-//            habitRepository.deleteById(delHabit.get().getId());
-//        }
-
-
-//        //find habit and delete tracker data - use when deleting a habit
-//       Iterable<Habit> aHabit = habitRepository.findAll();
-//       for (Habit eachHabit : aHabit) {
-//           if(eachHabit.getName().equals("second tracker test")) {
-//               Iterable<Tracker> aTracker = trackerRepository.findAll();
-//               for (Tracker eachTracker : aTracker) {
-//                   if(eachTracker.getHabit().getId() == (eachHabit.getId())) {
-//                       trackerRepository.deleteById(eachTracker.getId());
-//                   }
-//               }
-//           }
-//       }
-
-
-//        //test for finding a habit and saving new tracker - use for checkbox
-////        Iterable<Habit> aHabit = habitRepository.findAll();
-//       for (Habit eachHabit : aHabit) {
-//           if(eachHabit.getName().equals("second tracker test")) {
-//               Tracker newTracker = new Tracker();
-//               LocalDate testDate = LocalDate.of(2021, 6, 1);
-//               newTracker.setHabit(eachHabit);
-//               newTracker.setDate(testDate);
-//               trackerRepository.save(newTracker);
-//           }
-//       }
-
-
 
         //new instance of habit for add habit form
         model.addAttribute(new Habit());
@@ -125,34 +50,53 @@ public class HomeController {
         model.addAttribute("dates", dates);
         model.addAttribute("habits", habitRepository.findAll());
 
-        //testing checkboxes: https://frontbackend.com/thymeleaf/how-to-add-checked-attribute-to-input-conditionally-in-thymeleaf
-        model.addAttribute("flag", true);
+        //get data for checkboxes
+        Iterable<Habit> allHabits = habitRepository.findAll();
+        for (Habit habit : allHabits) {
+            System.out.println(habit);
+            ArrayList<Boolean> aHabit = new ArrayList<>();
+             for (LocalDate date : dates) {
+                 System.out.println(date);
+                 foundComplete = 0;
+                 System.out.println("reset to " + foundComplete);
+                 Iterable<Tracker> allTracker = trackerRepository.findAll();
+                 for (Tracker tracker : allTracker) {
+                     System.out.println(tracker.getHabit().getName() + " and " + tracker.getDate());
+                     System.out.println(tracker.getHabit().getId());
+                     System.out.println(habit.getId());
+                     System.out.println(tracker.getDate());
+                     System.out.println(date);
+                     if (tracker.getHabit().getId() == habit.getId() && tracker.getDate().compareTo(date) == 0) {
+                         foundComplete = 1;
+                         System.out.println("foundComplete set to : " + foundComplete);
+                         break;
+                     } else {
+                         System.out.println("Could not find this habit and date in tracker");
+                     }
+                 }
+
+                 if (foundComplete == 1) {
+                     aHabit.add(true);
+                     System.out.println("Has been set to true");
+                 } else if (foundComplete == 0) {
+                     aHabit.add(false);
+                     System.out.println("Has been set to false");
+                 }
+             }
+             System.out.println(aHabit);
+             isComplete.add(aHabit);
+        }
+
+        //get checkbox data
+        model.addAttribute("isComplete", isComplete);
 
         return "index";
     }
 
-//    @GetMapping
-//    public String addHabitForm(Model model) {
-//        model.addAttribute(new Habit());
-//        return "index";
-//    }
-
     @PostMapping
-    public String processAddHabit(@ModelAttribute Habit newHabit,  Model model) {
+    public String processAddHabit(@ModelAttribute Habit newHabit) {
         habitRepository.save(newHabit);
         return "redirect:";
     }
 
-
-
-//    @RequestMapping(value="")
-//    public String index(){
-//        return "index";
-//    }
-//
-//    @RequestMapping(value="goodbye")
-//    @ResponseBody
-//    public String goodbye(){
-//        return "Goodbye";
-//    }
 }
